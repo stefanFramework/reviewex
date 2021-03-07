adb10172d6064f577f2b0a30f76a2900d297d7fa
stefanFramework <cesar_cappetto@yahoo.com.ar>
2021-03-06 21:08:09 -0300

    Changing main colors and adding laravel-mix

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -1,17 +1,14 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.sass('resources/assets/css/style.scss', 'public/assets/css/style.css')
+    .sass('resources/assets/css/responsive.scss', 'public/assets/css/responsive.css')
+    .copy('resources/assets/css/animate.min.css', 'public/assets/css/animate.min.css', false)
+    .copy('resources/assets/css/bootstrap.min.css', 'public/assets/css/bootstrap.min.css', false)
+    .copy('resources/assets/css/boxicons.min.css', 'public/assets/css/boxicons.min.css', false)
+    .copy('resources/assets/css/flaticon.css', 'public/assets/css/flaticon.css', false)
+    .copy('resources/assets/css/meanmenu.min.css', 'public/assets/css/meanmenu.min.css', false)
+    .copy('resources/assets/css/owl.carousel.min.css', 'public/assets/css/owl.carousel.min.css', false)
+    .copy('resources/assets/css/owl.theme.default.min.css', 'public/assets/css/owl.theme.default.min.css', false)
+    .copy('resources/assets/js/', 'public/assets/js/', false)
+    .copy('resources/assets/images/', 'public/assets/images/', false)
+    .copy('resources/assets/fonts/', 'public/assets/fonts/', false);
